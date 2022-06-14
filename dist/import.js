3941f56d1f8e9e01f37ade9405dfa071fe11828f
binhnguyen288-source <74165689+binhnguyen288-source@users.noreply.github.com>
2022-06-14 18:39:28 +0700

    Supports clock, random

--- dist/import.js
+++ dist/import.js
@@ -1,9 +1,13 @@
 "use strict";
 async function updateUI() {
-    return new Promise(resolve => setTimeout(resolve));
+    return new Promise(resolve => setTimeout(resolve, 20));
 }
+const fileStr = '1 2 3';
+let filePtr = 0;
 async function load_wasm(wasm_binary) {
     let memory;
+    const consoleout = document.getElementById("consoleout");
+    const textDecoder = new TextDecoder();
     const imports = {
         env: {
             emscripten_notify_memory_growth: () => 0
@@ -12,27 +16,52 @@ async function load_wasm(wasm_binary) {
             proc_exit: (code) => {
                 throw code;
             },
-            fd_seek: () => { },
+            fd_seek: () => 0,
             fd_write: (fd, iovsPtr, iov, nwrittenPtr) => {
-                const memoryView = new DataView(memory.buffer);
+                const memU32 = new Uint32Array(memory.buffer);
                 let nwritten = 0;
-                let str = '';
+                for (let i = 0; i < iov; ++i) {
+                    const buf = memU32[(iovsPtr >> 2) + 2 * i];
+                    const size = memU32[(iovsPtr >> 2) + 2 * i + 1];
+                    nwritten += size;
+                    const jsbuf = new Uint8Array(memory.buffer, buf, size);
+                    consoleout.value += textDecoder.decode(jsbuf);
+                }
+                memU32[nwrittenPtr >> 2] = nwritten;
+                return 0;
+            },
+            fd_read: (fd, iovsPtr, iov, nreadPtr) => {
+                if (fd !== 3)
+                    throw "Error, reading an empty file";
+                const memoryView = new DataView(memory.buffer);
+                let nread = 0;
                 for (let i = 0; i < iov; ++i) {
                     const buf = memoryView.getUint32(iovsPtr + 8 * i, true);
                     const size = memoryView.getUint32(iovsPtr + 8 * i + 4, true);
-                    nwritten += size;
-                    const jsbuf = new Uint8Array(memory.buffer, buf, size);
-                    str += String.fromCharCode(...jsbuf);
+                    for (let j = 0; j < size && filePtr < fileStr.length; ++j, ++filePtr, ++nread) {
+                        memoryView.setUint8(buf + j, fileStr.charCodeAt(filePtr));
+                    }
                 }
-                memoryView.setUint32(nwrittenPtr, nwritten, true);
-                const consoleout = document.getElementById("consoleout");
-                consoleout.innerText += str;
+                memoryView.setUint32(nreadPtr, nread, true);
                 return 0;
             },
-            fd_read: () => { },
-            fd_close: () => { },
-            environ_sizes_get: () => 0,
-            environ_get: () => 0
+            fd_close: () => 0,
+            environ_sizes_get: (argcPtr, argvBufSizePtr) => {
+                const memU32 = new Uint32Array(memory.buffer);
+                memU32[argcPtr >> 2] = 0;
+                memU32[argvBufSizePtr >> 2] = 0;
+                return 0;
+            },
+            environ_get: () => 0,
+            random_get: (bufPtr, length) => {
+                window.crypto.getRandomValues(new Uint8Array(memory.buffer, bufPtr, length));
+                return 0;
+            },
+            clock_time_get: (id, precision, timePtr) => {
+                const memoryView = new DataView(memory.buffer);
+                memoryView.setBigUint64(timePtr, BigInt(Math.round(performance.now() * 1000000 / Number(precision))), true);
+                return 0;
+            }
         }
     };
     const { instance } = await WebAssembly.instantiate(wasm_binary, imports);
@@ -41,6 +70,6 @@ async function load_wasm(wasm_binary) {
         instance.exports._start();
     }
     catch (e) {
-        console.log(`Exited with code: ${e}`);
+        consoleout.value += `\nExited with code: ${e}`;
     }
 }
--- src/import.ts
+++ src/import.ts
@@ -1,10 +1,15 @@
 
 async function updateUI() {
-    return new Promise(resolve => setTimeout(resolve));
+    return new Promise(resolve => setTimeout(resolve, 20));
 }
+const fileStr = '1 2 3';
+let filePtr = 0;
+
 
 async function load_wasm(wasm_binary: ArrayBuffer) {
     let memory : WebAssembly.Memory;
+    const consoleout = document.getElementById("consoleout") as HTMLTextAreaElement;
+    const textDecoder = new TextDecoder();
     const imports = {
         env: {
             emscripten_notify_memory_growth: () => 0
@@ -13,36 +18,59 @@ async function load_wasm(wasm_binary: ArrayBuffer) {
             proc_exit: (code: number) => {
                 throw code;
             },
-            fd_seek: () => {},
+            fd_seek: () => 0,
             fd_write: (fd: number, iovsPtr: number, iov: number, nwrittenPtr: number): number => {
 
-                const memoryView = new DataView(memory.buffer);
+                const memU32 = new Uint32Array(memory.buffer);
 
                 let nwritten = 0;
-                let str = '';
+
                 for (let i = 0; i < iov; ++i) {
-                    const buf = memoryView.getUint32(iovsPtr + 8 * i, true);
-                    const size = memoryView.getUint32(iovsPtr + 8 * i + 4, true);
+                    const buf = memU32[(iovsPtr >> 2) + 2 * i];
+                    const size = memU32[(iovsPtr >> 2) + 2 * i + 1];
                     nwritten += size;
                     const jsbuf = new Uint8Array(memory.buffer, buf, size);
-                    str += String.fromCharCode(...jsbuf);
+                    consoleout.value += textDecoder.decode(jsbuf);
                 }
 
-                memoryView.setUint32(nwrittenPtr, nwritten, true);
-
-                const consoleout = document.getElementById("consoleout") as HTMLParagraphElement;
-                consoleout.innerText += str;
+                memU32[nwrittenPtr>>2] = nwritten;
 
                 return 0;
 
 
 
             },
-            fd_read: () => {},
-            fd_close: () => {},
-            environ_sizes_get: () => 0,
-            environ_get: () => 0
-
+            fd_read: (fd: number, iovsPtr: number, iov: number, nreadPtr: number) => {
+                if (fd !== 3) throw "Error, reading an empty file";
+                const memoryView = new DataView(memory.buffer);
+                let nread = 0;
+                for (let i = 0; i < iov; ++i) {
+                    const buf = memoryView.getUint32(iovsPtr + 8 * i, true);
+                    const size = memoryView.getUint32(iovsPtr + 8 * i + 4, true);
+                    for (let j = 0; j < size && filePtr < fileStr.length; ++j, ++filePtr, ++nread) {
+                        memoryView.setUint8(buf + j, fileStr.charCodeAt(filePtr));
+                    }
+                }
+                memoryView.setUint32(nreadPtr, nread, true);
+                return 0;
+            },
+            fd_close: () => 0,
+            environ_sizes_get: (argcPtr: number, argvBufSizePtr: number) => {
+                const memU32 = new Uint32Array(memory.buffer);
+                memU32[argcPtr >> 2] = 0;
+                memU32[argvBufSizePtr >> 2] = 0;
+                return 0;
+            },
+            environ_get: () => 0,
+            random_get: (bufPtr: number, length: number) => {
+                window.crypto.getRandomValues(new Uint8Array(memory.buffer, bufPtr, length));
+                return 0;
+            },
+            clock_time_get: (id: number, precision: bigint, timePtr: number) => {
+                const memoryView = new DataView(memory.buffer);
+                memoryView.setBigUint64(timePtr, BigInt(Math.round(performance.now() * 1000000 / Number(precision))), true);
+                return 0;
+            }
         }
     };
     const { instance } = await WebAssembly.instantiate(wasm_binary, imports);
@@ -54,6 +82,6 @@ async function load_wasm(wasm_binary: ArrayBuffer) {
         (instance.exports._start as Function)();
     }
     catch (e) {
-        console.log(`Exited with code: ${e}`)
+        consoleout.value += `\nExited with code: ${e}`;
     }
 }
